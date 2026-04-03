acdc1d9b522322b9d8b7540159fd3a436ad5d505
Kevin Messina <KMessina@CreativeApps.US>
2026-04-03 16:42:12 -0400

    Added dependy for TPPDF

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
             targets: ["CAS iOS Functions Library"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/techprimate/TPPDF", from: "2.6.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
