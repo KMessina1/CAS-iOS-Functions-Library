bf6a13ea76fa5984b9664f271288aa2ec13f6084
Kevin Messina <KMessina@CreativeApps.US>
2026-04-03 16:53:13 -0400

    fixed comma bug

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CAS iOS Functions Library"
+            name: "CAS iOS Functions Library",
             dependencies: [
                 .product(name: "TPPDF", package: "TPPDF")
             ]
